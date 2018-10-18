706a1aff9e8076d724e9503abb8a628518e755bf
Alvaro Miranda Aguilera <kikitux+github@gmail.com>
2018-10-18 09:45:53 +0200

    Update install-nginx.sh

--- install-nginx.sh
+++ install-nginx.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
-###Check if nginx is installed
+
 export DEBIAN_FRONTEND=noninteractive
-which nginx >/dev/null || {
-	sudo apt-get update
-	sudo apt-get install -y nginx
-	sudo apt-get clean
-	echo "Nginx has been instaled"
+
+# Check if nginx is installed
+# install if not
+which nginx &>/dev/null || {
+  sudo apt-get update
+  sudo apt-get install -y nginx
+  sudo apt-get clean
 }
